abcf53c9d571b4f6fc6abd4bc684a9c522a34f9b
Yadian Bacallao Gonzalez <yadianbg@gmail.com>
2023-06-08 14:21:59 -0400

    Generada la version 5.1.0 para recertificacion.

--- ActualizarBD/Properties/AssemblyInfo.cs
+++ ActualizarBD/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Empresa de Sistemas Automatizados - ALIMATIC")]
 [assembly: AssemblyProduct("Sicema Plus SQL")]
-[assembly: AssemblyCopyright("Copyright © 2022")]
+[assembly: AssemblyCopyright("Copyright © 2023")]
 [assembly: AssemblyTrademark("Grupo de Desarrollo de Sistemas")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.0.2.102")]
-[assembly: AssemblyFileVersion("5.0.2.102")]
+[assembly: AssemblyVersion("5.1.0.1")]
+[assembly: AssemblyFileVersion("5.1.0.1")]
